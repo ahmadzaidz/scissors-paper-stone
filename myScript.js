66406f3f9b2d4862dfae666ca9c58aa3f68b8a78
ahmad zaid <just.zaid@gmail.com>
2022-02-09 22:59:42 +0800

    successfully created playRound function

--- myScript.js
+++ myScript.js
@@ -1,6 +1,31 @@
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
-let 
 
-let computerChoice = 
+// 
+
+const options = ["scissors", "paper", "stone"];
+
+function random(number) {
+  return Math.floor(Math.random()*number)
+};  
+
+function computerPlay() {
+  
+  return options[random(3)];
+};
+
+const computerSelection = computerPlay();
+
+let playerPlay = window.prompt("choose scissors, paper or stone.");
+console.log("You chose " + playerPlay);
+console.log ("Computer chose " + computerSelection);
+let playerSelection = playerPlay.toLowerCase();
+
+function playRound() {
+if (playerSelection === computerSelection) {
+  console.log("It's a draw. ")
+}
+else if ((playerSelection==="scissors" && computerSelection==="paper") || (playerSelection==="paper" && computerSelection==="stone") || (playerSelection==="stone" && computerSelection==="scissors")){
+  console.log ("you won!")
+}
+else {console.log ("too bad, you lost.")}
+};
+playRound();
