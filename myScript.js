6b36418dec0cbbe24ef7075194888f240384cbf2
ahmad zaid <just.zaid@gmail.com>
2022-06-30 23:15:47 +0800

    scoring and final results work now

--- myScript.js
+++ myScript.js
@@ -1,9 +1,5 @@
 
-// 
-
 const options = ["scissors", "paper", "stone"];
-let computerScore = 0;
-let playerScore = 0;
 
 function random(number) {
   return Math.floor(Math.random()*number)
@@ -16,25 +12,41 @@ function computerPlay() {
 
 const computerSelection = computerPlay();
 
-let playerPlay = window.prompt("choose scissors, paper or stone.");
-const playerSelection = playerPlay.toLowerCase();
-console.log("You chose " + playerSelection);
-console.log ("Computer chose " + computerSelection);
+let computerScore = 0;
+let playerScore = 0;
 
+function game() {
+  function playRound() {
+  
+    let playerPlay = prompt("choose scissors, paper or stone.");
+    const playerSelection = playerPlay.toLowerCase();
+    computerPlay();
+    console.log("You chose " + playerSelection);
+    console.log ("Computer chose " + computerSelection);
+  
+    if (playerSelection === computerSelection) {
+      console.log("It's a draw. No change to the score")
+      }
+    else if ((playerSelection==="scissors" && computerSelection==="paper") || (playerSelection==="paper" && computerSelection==="stone") || 
+      (playerSelection==="stone" && computerSelection==="scissors")){
+      playerScore = playerScore + 1
+      console.log ("you won! Your score is " + playerScore + "! Computer score is " + computerScore + ".") 
+      }
+    else {
+      computerScore = computerScore + 1
+      console.log ("too bad, you lost. Your score is " + playerScore + "! Computer score is " + computerScore + ".")}
+      };
+  for (let i = 0; i<5; i++) {
+    playRound()};   
 
-function playRound() {
-if (playerSelection === computerSelection) {
-  console.log("It's a draw. No change to the score")
-}
-else if ((playerSelection==="scissors" && computerSelection==="paper") || (playerSelection==="paper" && computerSelection==="stone") || 
-(playerSelection==="stone" && computerSelection==="scissors")){
-  return playerScore++;
-  console.log ("you won! Your score is " + playerScore + "! Computer score is " + computerScore) 
-}
-else {
-  return computerScore++;
-  console.log ("too bad, you lost. Your score is " + playerScore + "! Computer score is " + computerScore)}
+  if (playerScore === computerScore) {
+    console.log("eh draw la sia. Your score is " + playerScore + "! Computer score is " + computerScore + ".")
+  }
+  else if (playerScore > computerScore) {
+    console.log("woohoo you won! Your score is " + playerScore + "! Computer score is " + computerScore + ".")
+  }
+  else {console.log("nooo, you lost... Your score is " + playerScore + "! Computer score is " + computerScore + ".")};
 };
 
-playRound();
+game();
 
