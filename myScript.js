61dc393f0813ef638906eda86f8a6d94533734dc
ahmad zaid <just.zaid@gmail.com>
2022-06-30 23:48:38 +0800

    reworked to return playerSelection and computerSelection as well as use template literals

--- myScript.js
+++ myScript.js
@@ -10,12 +10,53 @@ function computerPlay() {
   return options[random(3)];
 };
 
-const computerSelection = computerPlay();
+// const computerSelection = computerPlay();
 
 let computerScore = 0;
 let playerScore = 0;
 
+
+
+function playRound(playerSelection, computerSelection) {
+
+playerSelection = prompt("choose scissors, paper or stone.")
+playerSelection = playerSelection.toLowerCase();
+computerSelection = computerPlay(); 
+
+  if (playerSelection === computerSelection) {
+    console.log(`It's a draw. Both picked ${playerSelection}. No change to the score`)
+    }
+  else if ((playerSelection==="scissors" && computerSelection==="paper") || (playerSelection==="paper" && computerSelection==="stone") || 
+    (playerSelection==="stone" && computerSelection==="scissors")){
+    playerScore = playerScore + 1
+    console.log (`you won! Your ${playerSelection} beats their ${computerSelection}. Your score is ${playerScore}! Computer score is ${computerScore}.`) 
+    }
+  else {
+    computerScore = computerScore + 1
+    console.log (`too bad, you lost. Their ${computerSelection} beats your ${playerSelection}. Your score is ${playerScore}! Computer score is ${computerScore}.`)}
+    };
+
 function game() {
+  for (let i = 0; i<5; i++) {
+    playRound()};   
+  
+  if (playerScore === computerScore) {
+      console.log("eh draw la sia. Your score is " + playerScore + "! Computer score is " + computerScore + ".")
+      }
+      else if (playerScore > computerScore) {
+      console.log("woohoo you won! Your score is " + playerScore + "! Computer score is " + computerScore + ".")
+      }
+      else {console.log("nooo, you lost... Your score is " + playerScore + "! Computer score is " + computerScore + ".")
+      };
+   };
+
+game();
+
+
+
+
+
+/* function game() {
   function playRound() {
   // this prompts player selection
     let playerPlay = prompt("choose scissors, paper or stone.");
@@ -41,7 +82,7 @@ function game() {
     for (let i = 0; i<5; i++) {
     playRound()};   
 
-  // this is the final score message
+  // this is the final score message. take note that "=" made it set the variable and draw all the time, changing it to "===" works.
     if (playerScore === computerScore) {
     console.log("eh draw la sia. Your score is " + playerScore + "! Computer score is " + computerScore + ".")
     }
@@ -54,3 +95,4 @@ function game() {
 //this plays the game 
 game();
 
+*/
